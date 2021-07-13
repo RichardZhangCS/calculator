6658f41074907d9fed2b25def19f5101fb689eaf
RichardZhangCS <richardzhang1303@gmail.com>
2021-07-12 21:58:04 -0500

    Added basic calculator functions

--- main.js
+++ main.js
@@ -4,11 +4,53 @@ function multiply(a,b) {return a*b;};
 function divide(a,b) {return a/b;};
 
 function operate(operation, a, b) {
-    return operation(a+b);
+    return operation(a,b);
 }
 
 const container = document.querySelector(".container");
 const calculatorButtons = container.querySelectorAll(".calc-button");
-calculatorButtons.forEach((button) => {
+let previousValue;
+let operation;
+
+function buttonMasterFunction() {
+    const displayScreen = container.querySelector("#display-screen");
+    const symbol = this.textContent;
+    if (symbol >= '0' && symbol <= '9') {
+        displayScreen.value += symbol;
+        return;
+    }
+    if (symbol=="CLEAR") {
+        displayScreen.value = '';
+        previousValue = null;
+        return;
+    }
+    if (symbol=="=") {
+        if (!previousValue) return;
+        const result = operate(operation, parseInt(previousValue), parseInt(displayScreen.value));
+        previousValue = result;
+        displayScreen.value = result;
+        return;
+    }
+
     
+    switch (symbol) {
+        case '+':
+            operation = add;
+            break;
+        case '-':
+            operation = subtract;
+            break;
+        case '*':
+            operation = multiply;
+            break;
+        case '/':
+            operation = divide;
+            break;
+    }
+    previousValue = displayScreen.value;
+    displayScreen.value = '';
+}
+
+calculatorButtons.forEach((button) => {
+    button.addEventListener('click', buttonMasterFunction);
 });
